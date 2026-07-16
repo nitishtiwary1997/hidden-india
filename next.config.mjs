6059892ca49ccd5eab31c05e7d37face9b5a34c3
Nitish Kumar tiwary <madeeasy@Nitishs-MacBook-Air.local>
2026-07-16 23:06:43 +0530

    Initial commit of Hidden India travel website

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   typescript: {
-//     ignoreBuildErrors: true,
-//   },
-// };
-
-// export default nextConfig;
-
-
 const nextConfig = {
-  output: 'export', // Static pages generate karne ke liye
-  images: {
-    unoptimized: true, // GitHub Pages ke liye image optimization off
-  },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 };
+
+export default nextConfig;
